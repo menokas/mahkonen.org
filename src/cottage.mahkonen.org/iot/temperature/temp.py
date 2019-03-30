5c97bde9faede8df5becd90a9ebc8d8a05759432
Ubuntu <ubuntu@ubuntu.attlocal.net>
2019-03-30 20:24:50 +0000

    python3 changes

--- src/cottage.mahkonen.org/iot/temperature/temp.py
+++ src/cottage.mahkonen.org/iot/temperature/temp.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 import serial
 import io
 
 ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-sio.write(unicode("A"))
+sio.write(str("A"))
 sio.flush()
 temperature = sio.read().strip()
-print temperature
+print(temperature)
